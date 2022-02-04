e28748937d0a1968bacc98a74a2b0cc799da9d37
Musa <musaabdullah.hundur@gmail.com>
2022-02-04 17:58:47 +0300

    survivorbird

---
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -0,0 +1,60 @@
+package com.atilsamancioglu.survivorbirdstarter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SurvivorBird extends ApplicationAdapter {
+
+	SpriteBatch batch;
+	Texture background;
+	Texture bird;
+	float birdX = 0;
+	float birdY = 0;
+	int gameState = 0;
+	float velocity = 0;
+	float gravity = 0.1f;
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("background.png");
+		bird = new Texture("bird.png");
+		birdX = Gdx.graphics.getWidth()/4;
+		birdY = Gdx.graphics.getHeight()/2;
+
+	}
+
+	@Override
+	public void render () {
+		if (Gdx.input.justTouched()){
+			gameState = 1;
+		}
+
+		if (gameState == 1){
+			if (birdY>0 || velocity <0){
+				velocity = velocity + gravity;
+				birdY = birdY - velocity;
+			}
+		}else if (gameState == 0){
+			if (Gdx.input.justTouched()){
+				gameState = 1;
+			}
+		}
+
+		batch.begin();
+		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(bird,birdX,birdY,Gdx.graphics.getWidth()/15,Gdx.graphics.getHeight()/10);
+
+
+		batch.end();
+	}
+
+	@Override
+	public void dispose () {
+
+	}
+}
