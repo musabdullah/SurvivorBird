bbec95f56e27aecd580a1f8b185b06d5b7dae4f0
Musa <musaabdullah.hundur@gmail.com>
2022-02-04 18:10:14 +0300

    update

--- core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
+++ core/src/com/atilsamancioglu/survivorbirdstarter/SurvivorBird.java
@@ -30,11 +30,13 @@ public class SurvivorBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		if (Gdx.input.justTouched()){
-			gameState = 1;
-		}
 
 		if (gameState == 1){
+
+			if (Gdx.input.justTouched()){
+				velocity = -6;
+			}
+
 			if (birdY>0 || velocity <0){
 				velocity = velocity + gravity;
 				birdY = birdY - velocity;
